9b20214a8225b29754ab5d1878230b37a9f70bda
koromu <139294026+koromu@users.noreply.github.com>
2023-10-09 23:20:52 +0800

    Add files via upload

---
+++ Project1/刷题/P5710 【深基3.例2】数的性质.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+//еп╤о
+//int jud(int x)
+//{
+//    int count = 0;
+//    if (x % 2 == 0)
+//    {
+//        count++;
+//    }
+//    if (x >= 4 && x <= 12)
+//    {
+//        count++;
+//    }
+//    return count;
+//
+//}
+//
+//int main()
+//{
+//    int x;
+//    scanf("%d", &x);
+//    int A = 0, U = 0, B = 0, Z = 0;
+//    if (jud(x) == 2)
+//        A = 1;
+//    if (jud(x) == 1)
+//        B = 1;
+//    if (jud(x) >= 1)
+//        U = 1;
+//    if (jud(x) == 0)
+//        Z = 1;
+//    printf("%d %d %d %d", A, U, B, Z);
+//    return 0;
+//}
