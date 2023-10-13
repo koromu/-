9bb98f39dbe60251471cace6189f71867c666a69
koromu <139294026+koromu@users.noreply.github.com>
2023-10-13 22:11:45 +0800

    Update P1980 [NOIP2013 普及组] 计数问题.cpp

--- Project1/刷题/P1980 [NOIP2013 普及组] 计数问题.cpp
+++ Project1/刷题/P1980 [NOIP2013 普及组] 计数问题.cpp
@@ -1,20 +1,20 @@
-//#include <iostream>
-//using namespace std;
-//typedef long long ll;
-//int main()
-//{
-//	ll n, x, c=0, n1, n2; cin >> n >> x;//c记录出现次数
-//	for (; n > 0; n--)
-//	{
-//		n1 = n2 = n;
-//		//进入判断, n2用来处理判断
-//		do
-//		{
-//			n2 = n1 % 10;//得到个位
-//			if (n2 == x)
-//				c++;
-//		} while (n1 /= 10);
-//	}
-//	cout << c;
-//	return 0;
-//}
+#include <iostream>
+using namespace std;
+typedef long long ll;
+int main()
+{
+	ll n, x, c=0, n1, n2; cin >> n >> x;//c璁板綍鍑虹幇娆℃暟
+	for (; n > 0; n--)
+	{
+		n1 = n2 = n;
+		//杩涘叆鍒ゆ柇, n2鐢ㄦ潵澶勭悊鍒ゆ柇
+		do
+		{
+			n2 = n1 % 10;//寰楀埌涓綅
+			if (n2 == x)
+				c++;
+		} while (n1 /= 10);
+	}
+	cout << c;
+	return 0;
+}
