68c346934714f81182bed76d588eea2e1885b04d
koromu <139294026+koromu@users.noreply.github.com>
2023-10-13 22:08:37 +0800

    Add files via upload

---
+++ Project1/刷题/P5709 【深基2.习6】Apples Prologue .cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int m, t, s;
+	cin >> m >> t >> s;
+	if (t == 0)
+	{
+		cout << 0;
+		return 0;
+	}
+	//有可能早就吃完了
+	m = m - s / t;
+	//有可能不满一个, 也要当一个算, 先判断是否能除尽, 除不尽就+1
+	if (m <= 0)
+	{
+		cout << 0;
+		return 0;
+	}
+	if (s % t == 0)
+		cout << m;
+	else
+		cout << m - 1;
+	return 0;
+}
---
+++ Project1/刷题/P5721 【深基4.例6】数字直角三角形.cpp
@@ -0,0 +1,26 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//void Pri(const int x)
+//{
+//	if (x / 10 == 0) cout << 0 << x;
+//	else cout << x;
+//}
+//
+//int main()
+//{
+//	int n; cin >> n;
+//	int i, j, x=1;
+//	for (i = n ;i > 0; i--)
+//	{
+//		for (j = i; j > 0; j--)
+//		{
+//			Pri(x);//判断单数还是双数
+//			x++;
+//		}
+//		cout << endl;
+//	}
+//
+//	return 0;
+//}
