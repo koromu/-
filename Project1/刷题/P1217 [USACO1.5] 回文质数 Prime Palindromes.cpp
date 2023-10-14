9e61b23e52a632b159bd83ecaf1dc6b613abe3fb
koromu <139294026+koromu@users.noreply.github.com>
2023-10-14 23:49:21 +0800

    Update P1217 [USACO1.5] 回文质数 Prime Palindromes.cpp

--- Project1/刷题/P1217 [USACO1.5] 回文质数 Prime Palindromes.cpp
+++ Project1/刷题/P1217 [USACO1.5] 回文质数 Prime Palindromes.cpp
@@ -1,87 +1,87 @@
-//using namespace std;
-//#include <iostream>
-//#include <vector>
-//typedef long long ll;
-//
-//
-//int jud2(ll i)
-//{//ÕÒµ½ÆæÊı
-//	if (i % 2 != 0)
-//		return 1;
-//	else 
-//		return 0;
-//}
-//
-//int jud3(ll i)
-//{//ÕÒµ½ËØÊı
-//	//ÕÒµ½ÆæÊıµÄÖĞ¼äÊı+1(ÒÔ·ÀÍòÒ»),ÓÃÕâ¸öÊı³ıÒÔ´Ó1 - ÖĞ¼äÊı, ²»±»Õû³ı¾ÍÊÇËØÊı
-//	ll mid = i / 2 + 1 ;
-//	for(int j = 2; j < mid ; j++)
-//	{
-//		if (i % j == 0)
-//		{
-//			return 0;
-//		}
-//	}
-//	return 1;
-//}
-//
-//int Pow(int x, int y)
-//{
-//	int temp = 1;
-//	for (int i = 0; i < y ; i++)
-//	{
-//		temp = temp * x;
-//	}
-//	return temp;
-//}
-//
-//int jud5(int i)
-//{//ÅĞ¶ÏÎ»Êı
-//	int c = 0;
-//	while (i > 0)
-//	{
-//		i /= 10;
-//		c++;
-//	}
-//	return c;
-//}
-//
-//int jud4(ll i)
-//{//ÅĞ¶ÏÊÇ·ñ»ØÎÄ, »ØÎÄÊı¿ÉÒÔ°ÑÒ»¸öÊıµ¹¹ıÀ´ÅĞ¶ÏÊÇ·ñÏàµÈ
-//	int temp1 = 0,temp2 = i, c = jud5(i);//c¼ÇÂ¼Î»Êı
-//	while (temp2 > 0)
-//	{
-//		temp1 += (temp2 % 10) * Pow(10,c-1);
-//		temp2 /= 10;
-//		c--;
-//	}
-//	if (temp1 == i)
-//	{
-//		return 1;
-//	}
-//	else return 0;
-//
-//}
-//
-//int main()
-//{
-//	//ÏÈÕÒËØÊı, ÔÙÅĞ¶ÏÊÇ·ñ»ØÎÄ
-//	//ËØÊıÊÇÆæÊı ÆæÊı²»Ò»¶¨ÊÇËØÊı
-//	ll a, b;
-//	cin >> a >> b;
-//	for (ll i = a; i < b; i++)//´Óa-b
-//	{
-//		if (jud2(i))
-//		{
-//			if (jud3(i))
-//			{
-//				if (jud4(i))
-//				{
-//					cout << i << endl;
-//				}
-//			}
-//		}
-//	}
-//	return 0;
-//}
+using namespace std;
+#include <iostream>
+#include <vector>
+typedef long long ll;
+
+
+int jud2(ll i)
+{//æ‰¾åˆ°å¥‡æ•°
+	if (i % 2 != 0)
+		return 1;
+	else 
+		return 0;
+}
+
+int jud3(ll i)
+{//æ‰¾åˆ°ç´ æ•°
+	//æ‰¾åˆ°å¥‡æ•°çš„ä¸­é—´æ•°+1(ä»¥é˜²ä¸‡ä¸€),ç”¨è¿™ä¸ªæ•°é™¤ä»¥ä»1 - ä¸­é—´æ•°, ä¸è¢«æ•´é™¤å°±æ˜¯ç´ æ•°
+	ll mid = i / 2 + 1 ;
+	for(int j = 2; j < mid ; j++)
+	{
+		if (i % j == 0)
+		{
+			return 0;
+		}
+	}
+	return 1;
+}
+
+int Pow(int x, int y)
+{
+	int temp = 1;
+	for (int i = 0; i < y ; i++)
+	{
+		temp = temp * x;
+	}
+	return temp;
+}
+
+int jud5(int i)
+{//åˆ¤æ–­ä½æ•°
+	int c = 0;
+	while (i > 0)
+	{
+		i /= 10;
+		c++;
+	}
+	return c;
+}
+
+int jud4(ll i)
+{//åˆ¤æ–­æ˜¯å¦å›æ–‡, å›æ–‡æ•°å¯ä»¥æŠŠä¸€ä¸ªæ•°å€’è¿‡æ¥åˆ¤æ–­æ˜¯å¦ç›¸ç­‰
+	int temp1 = 0,temp2 = i, c = jud5(i);//cè®°å½•ä½æ•°
+	while (temp2 > 0)
+	{
+		temp1 += (temp2 % 10) * Pow(10,c-1);
+		temp2 /= 10;
+		c--;
+	}
+	if (temp1 == i)
+	{
+		return 1;
+	}
+	else return 0;
+
+}
+
+int main()
+{
+	//å…ˆæ‰¾ç´ æ•°, å†åˆ¤æ–­æ˜¯å¦å›æ–‡
+	//ç´ æ•°æ˜¯å¥‡æ•° å¥‡æ•°ä¸ä¸€å®šæ˜¯ç´ æ•°
+	ll a, b;
+	cin >> a >> b;
+	for (ll i = a; i < b; i++)//ä»a-b
+	{
+		if (jud2(i))
+		{
+			if (jud3(i))
+			{
+				if (jud4(i))
+				{
+					cout << i << endl;
+				}
+			}
+		}
+	}
+	return 0;
+}
