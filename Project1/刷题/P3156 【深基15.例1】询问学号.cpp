d0144016801d162ad73a2fdf5b551ce029d48400
koromu <139294026+koromu@users.noreply.github.com>
2023-10-14 23:48:54 +0800

    Update P3156 【深基15.例1】询问学号.cpp

--- Project1/刷题/P3156 【深基15.例1】询问学号.cpp
+++ Project1/刷题/P3156 【深基15.例1】询问学号.cpp
@@ -1,30 +1,30 @@
-//#include <iostream>
-//#include <vector>
-//using namespace std;
-//typedef long long ll;
-//
-//int main()
-//{
-//	vector<int> id;
-//	ll n, m;
-//	cin >> n >> m;
-//	for (int i = 0; i < n; i++)
-//	{
-//		int temp; cin >> temp;
-//		id.push_back(temp);
-//	}
-//	//for (int i = 0; i < m; i++)
-//	//{
-//	//	int temp; cin >> temp;
-//	//	auto it = id.begin();
-//	//	for (int j = 1; j < temp; j++)
-//	//		it++;
-//	//	cout << *it << endl;
-//	//}
-//	for (int i = 0; i < m; i++)
-//	{
-//		int temp; cin >> temp;
-//		cout << id[temp - 1] << endl;
-//	}
-//	return 0;
-//}
+#include <iostream>
+#include <vector>
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+	vector<int> id;
+	ll n, m;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+	{
+		int temp; cin >> temp;
+		id.push_back(temp);
+	}
+	//for (int i = 0; i < m; i++)
+	//{
+	//	int temp; cin >> temp;
+	//	auto it = id.begin();
+	//	for (int j = 1; j < temp; j++)
+	//		it++;
+	//	cout << *it << endl;
+	//}
+	for (int i = 0; i < m; i++)
+	{
+		int temp; cin >> temp;
+		cout << id[temp - 1] << endl;
+	}
+	return 0;
+}
