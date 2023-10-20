469d61e10b5277e3d7091116fc337ee8328858d5
koromu <139294026+koromu@users.noreply.github.com>
2023-10-20 23:44:30 +0800

    Add files via upload

---
+++ Project1/刷题/P1914 小书童——凯撒密码.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	//每个字母向后移动一位,特殊z后面是a
+	int n; cin >> n;
+	string s; cin >> s;
+	n = n % 26;//范围是0 - 25,如果是26就等于没变
+	for (int i = 0; i < s.size(); i++)
+	{
+		if (s[i] + n > 'z')//先加,再减去26
+			s[i] = s[i] + n - 26;
+		else
+			s[i] += n;
+	}
+	cout << s;
+	return 0;
+}
---
+++ Project1/刷题/P5733 【深基6.例1】自动修正.cpp
@@ -0,0 +1,16 @@
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//int main()
+//{
+//	string s; cin >> s;
+//	for (int i = 0; i < s.size(); i++)
+//	{
+//		if (s[i] >= 'a' && s[i] <= 'z')//a - A左闭右开
+//			s[i] -= 32;//不用背的差值
+//	}
+//	cout << s;
+//
+//	return 0;
+//}
