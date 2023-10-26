6811c36fcdaf0506536464f44e57a5811eeb7065
koromu <139294026+koromu@users.noreply.github.com>
2023-10-26 16:26:33 +0800

    Update c实验2顺序结构程序设计.cpp

--- Project1/实验课代码/c实验2顺序结构程序设计.cpp
+++ Project1/实验课代码/c实验2顺序结构程序设计.cpp
@@ -1,88 +1,88 @@
-//#define _CRT_SECURE_NO_WARNINGS
-//#include<stdio.h>
-//int main()
-//{
-//	int x, y;
-//	char ch1, ch2, ch3;
-//	scanf("%d,%d", &x, &y);
-//	getchar();                           //µÃµ½scanfÓï¾äÊäÈëµÄ»Ø³µ·û
-//	printf("x=%d,y=%d\n", x, y);
-//	scanf("%c%c%c", &ch1, &ch2, &ch3);
-//	printf("ch1=%c,ch2=%c,ch3=%c\n", ch1, ch2, ch3);
-//	return 0;
-//}
-//
-//#define _CRT_SECURE_NO_WARNINGS
-//#include<stdio.h>
-//int main()
-//{
-//	char X;
-//	scanf("%c", &X);
-//	X -= 32;
-//	printf("%c\n", X);
-//	return 0;
-//}
-//
-//#define _CRT_SECURE_NO_WARNINGS
-//#include<stdio.h>
-//#include <math.h>
-//int main()
-//{
-//	int a, b, c, S = 0;	scanf("%d %d %d", &a, &b, &c);
-//	int s = (a + b + c) / 2;
-//	S = sqrt(s*((s - a) * (s - b) * (s - c)));
-//	printf("%d", S);
-//	return 0;
-//}
-//#define _CRT_SECURE_NO_WARNINGS
-//#include<stdio.h>
-//#include <math.h>
-//int main()
-//{
-//	float r = 0.0, h = 0.0, PI = 3.14, S1, S2, V;
-//	scanf("%f %f", &r, &h);
-//	S1 = PI * r * r;//µ×Ãæ»ı
-//	S2 = S1 + 2*PI*r*h; //±íÃæ»ı
-//	V = S1 * h; // Ìå»ı
-//	printf("Ô²Öùµ×Ãæ»ı = %.2f, Ô²Öù±íÃæ»ı = %.2f, Ô²ÖùÌå»ı = %.2f\n", S1, S2, V);
-//	return 0;
-//}
-//#define _CRT_SECURE_NO_WARNINGS
-//#include<stdio.h>
-//#include <math.h>
-//int main()
-//{//´æ·ÅÒ»¸öXÎ»Êı
-//	int x; scanf("%d", &x);
-//	int temp = 0;
-//	while (x != 0)
-//	{
-//		temp = x % 10;
-//		printf("%d", temp);
-//		x /= 10;
-//	}
-//		return 0;
-//}
-//#define _CRT_SECURE_NO_WARNINGS
-//#include<stdio.h>
-//#include <math.h>
-//void swap1(int* a, int* b)
-//{
-//	*a = *a + *b;
-//	*b = *a - *b;
-//	*a = *a - *b;
-//}
-//void swap2(int* a,int* b)
-//{
-//	int temp;
-//	temp = *a;
-//	*a = *b;
-//	*b = temp;
-//}
-//int main()
-//{
-//	int a, b; scanf("%d %d", &a, &b);
-//	swap1(&a, &b);//²»½èÖúÆäËû±äÁ¿
-//	//swap2(&a, &b);//½èÖúÆäËû±äÁ¿
-//	printf("a = %d b = %d", a, b);
-//	return 0;
-//}
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int x, y;
+	char ch1, ch2, ch3;
+	scanf("%d,%d", &x, &y);
+	getchar();                           //å¾—åˆ°scanfè¯­å¥è¾“å…¥çš„å›è½¦ç¬¦
+	printf("x=%d,y=%d\n", x, y);
+	scanf("%c%c%c", &ch1, &ch2, &ch3);
+	printf("ch1=%c,ch2=%c,ch3=%c\n", ch1, ch2, ch3);
+	return 0;
+}
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	char X;
+	scanf("%c", &X);
+	X -= 32;
+	printf("%c\n", X);
+	return 0;
+}
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include <math.h>
+int main()
+{
+	int a, b, c, S = 0;	scanf("%d %d %d", &a, &b, &c);
+	int s = (a + b + c) / 2;
+	S = sqrt(s*((s - a) * (s - b) * (s - c)));
+	printf("%d", S);
+	return 0;
+}
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include <math.h>
+int main()
+{
+	float r = 0.0, h = 0.0, PI = 3.14, S1, S2, V;
+	scanf("%f %f", &r, &h);
+	S1 = PI * r * r;//åº•é¢ç§¯
+	S2 = S1 + 2*PI*r*h; //è¡¨é¢ç§¯
+	V = S1 * h; // ä½“ç§¯
+	printf("åœ†æŸ±åº•é¢ç§¯ = %.2f, åœ†æŸ±è¡¨é¢ç§¯ = %.2f, åœ†æŸ±ä½“ç§¯ = %.2f\n", S1, S2, V);
+	return 0;
+}
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include <math.h>
+int main()
+{//å­˜æ”¾ä¸€ä¸ªXä½æ•°
+	int x; scanf("%d", &x);
+	int temp = 0;
+	while (x != 0)
+	{
+		temp = x % 10;
+		printf("%d", temp);
+		x /= 10;
+	}
+		return 0;
+}
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include <math.h>
+void swap1(int* a, int* b)
+{
+	*a = *a + *b;
+	*b = *a - *b;
+	*a = *a - *b;
+}
+void swap2(int* a,int* b)
+{
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+int main()
+{
+	int a, b; scanf("%d %d", &a, &b);
+	swap1(&a, &b);//ä¸å€ŸåŠ©å…¶ä»–å˜é‡  
+	//swap2(&a, &b);//å€ŸåŠ©å…¶ä»–å˜é‡
+	printf("a = %d b = %d", a, b);
+	return 0;
+}
